751311ccb642b7ccdeb38821cef0e08206170994
ninomyaovo <151879976+ninomyaovo@users.noreply.github.com>
2025-05-24 12:30:12 -0700

    Update curl_test.sh

--- curl_test.sh
+++ curl_test.sh
@@ -1,4 +1,12 @@
+#Test in local
 curl -H "Content-Type: application/json" \
      -X POST \
      -d '{"cyl":6,"disp":160,"hp":110,"drat":3.9,"wt":2.62,"qsec":16.46,"vs":0,"am":1,"gear":4,"carb":4}' \
      http://localhost:5050/predict_mpg
+
+#Test on google cloud run
+curl -H "Content-Type: application/json" \
+     -X POST \
+     -d '{"cyl":6,"disp":160,"hp":110,"drat":3.9,"wt":2.62,"qsec":16.46,"vs":0,"am":1,"gear":4,"carb":4}' \
+     https://mtcars-flask-app-696925248391.europe-west1.run.app/predict_mpg
+
